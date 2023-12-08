11bf9a3fe52127ed390448a2752976f24170c336
Harun Yılmaz <haruny232@aof.anadolu.edu.tr>
2023-12-08 18:30:19 +0300

    payment deleted

--- src/main/java/com/store/store/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.store.store.repository;
-
-import com.store.store.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
