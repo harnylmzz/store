75f5035099eaf1be2208713b58e35de2f726d247
Harun Yılmaz <haruny232@aof.anadolu.edu.tr>
2023-12-11 17:00:12 +0300

    address derived query added

--- src/main/java/com/store/store/repository/AddressRepository.java
+++ src/main/java/com/store/store/repository/AddressRepository.java
@@ -4,6 +4,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.store.store.model.Address;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address, Integer> {
-    
+
+    List<Address> findByCountry(String country);
+
+    List<Address> findByCity(String city);
+
+    List<Address> findByCountryAndCity(String country, String city);
+
+    List<Address> findByZipCode(String zipCode);
+
+    List<Address> findByCountryStartingWith(String country);
+
+    List<Address> findByCityStartingWith(String city);
+
+    List<Address> findByCountryEndingWith(String country);
+
+    List<Address> findByCityEndingWith(String city);
+
+    List<Address> findByCountryContaining(String country);
+
+    List<Address> findByCityContaining(String city);
+
+    List<Address> findByCountryAndCityContaining(String country, String city);
+
+    List<Address> findByCountryAndCityContainingStartingWith(String country, String city);
+
+    List<Address> findByCountryAndCityContainingEndingWith(String country, String city);
+
 }
